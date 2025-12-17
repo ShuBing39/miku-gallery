3df7521cc9ba1e93a86144ab51fdaa99ddc6f316
Shubing39 <wxjwxjk@qq.com>
2025-12-17 11:02:05 +0800

    修复无法注册 真的修复了吗

--- src/supabase.js
+++ src/supabase.js
@@ -3,4 +3,5 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
+// 创建单例客户端
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
